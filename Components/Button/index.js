7c22f889fabd4f315149d22b68b299dc01f69797
Christopher Barros <christopherbarros@ChristophersMBP.home>
2020-10-29 22:56:29 -0400

    added dissabled option for btn

--- Components/Button/index.js
+++ Components/Button/index.js
@@ -2,14 +2,23 @@ import React from "react";
 
 import styles from "./styles.module.css";
 
-const Button = ({ image, className, label, value, onClick }) => {
+const Button = ({
+  image,
+  className,
+  label,
+  value,
+  onClick,
+  disabled,
+  type,
+}) => {
   return (
     <div>
       {label ? <div>{label}</div> : null}
       <button
         onClick={onClick}
-        src={image ? image : null}
-        className={styles.btn}
+        src={image}
+        className={`${styles.btn} ${className}`}
+        disabled={disabled}
       >
         {value ? value : null}
       </button>
